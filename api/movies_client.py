ed52bdf4103d88a615bb64e40d9adb66b0ecde61
sunil1097 <sunilsamal9770406@gmail.com>
2025-12-29 16:11:42 +0530

    add data driven movie tests

--- api/movies_client.py
+++ api/movies_client.py
@@ -1,4 +1,4 @@
 import requests
 
-def get(base_url,params):
+def get_movie(base_url,params):
     return requests.get(url=base_url,params=params)
--- tests/test_movies.py
+++ tests/test_movies.py
@@ -1,15 +1,15 @@
-from api.client import get
-from utils.schema_validator import validate_schema
-def test_get_movies_by_title(base_url,api_key):
+import pytest
+from api.movies_client import get_movie
+from utils.data_loader import load_test_data
+test_data =load_test_data("data/movies.json")
+
+@pytest.mark.parametrize("movie",test_data)
+def test_get_movies_by_title(base_url,api_key,movie):
     params ={
         "apikey":api_key,
-        "t":"Inception"
+        "t":movie["title"]
     }
-    response = get(base_url,params)
-    assert response.status_code==200
-    body =response.json()
-    print("Response Body:",body)
-    validate_schema(body,"schemas/movie_schema.json")
-    assert body["Title"]=="Inception"
-    assert body["Response"]=="True"
+    response =get_movie(base_url,params)
+    body=response.json()
 
+    assert body["Response"]== movie["expected"]
