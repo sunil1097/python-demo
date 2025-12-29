d6a346c4472f7063799d15c355143211758cac8a
sunil1097 <sunilsamal9770406@gmail.com>
2025-12-29 16:33:59 +0530

    changes in schema validation

--- tests/test_movies.py
+++ tests/test_movies.py
@@ -1,15 +1,28 @@
 import pytest
 from api.movies_client import get_movie
 from utils.data_loader import load_test_data
+from utils.schema_validator import validate_schema
 test_data =load_test_data("data/movies.json")
 
 @pytest.mark.parametrize("movie",test_data)
-def test_get_movies_by_title(base_url,api_key,movie):
+def test_get_movies_by_title(base_url,api_key,movie,request):
+    #dyamically apply marker 
+
+    request.node.add_marker(getattr(pytest.mark,movie["type"]))
     params ={
         "apikey":api_key,
         "t":movie["title"]
     }
-    response =get_movie(base_url,params)
-    body=response.json()
+    response = get_movie(base_url,params)
+    body =response.json()
 
-    assert body["Response"]== movie["expected"]
+    #schema validation 
+    validate_schema(body,movie["schema"])
+
+    #business assertions 
+    assert body["Response"] == movie["expected_response"]
+
+    #error validation (only if expected )
+    if movie ["expected_response"]== "False":
+        assert body["Error"] == movie["expected_error"]
+   
