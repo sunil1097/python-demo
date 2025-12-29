4cd58dc32f7f3850d65fb3379945feb6f28e6543
sunil1097 <sunilsamal9770406@gmail.com>
2025-12-29 16:37:46 +0530

    remove old code

--- tests/test_movies.py
+++ tests/test_movies.py
@@ -1,28 +1,29 @@
 import pytest
 from api.movies_client import get_movie
-from utils.data_loader import load_test_data
+from utils.data_loader import load_json_data
 from utils.schema_validator import validate_schema
-test_data =load_test_data("data/movies.json")
 
-@pytest.mark.parametrize("movie",test_data)
-def test_get_movies_by_title(base_url,api_key,movie,request):
-    #dyamically apply marker 
+test_data = load_json_data("data/movies.json")
 
-    request.node.add_marker(getattr(pytest.mark,movie["type"]))
-    params ={
-        "apikey":api_key,
-        "t":movie["title"]
+@pytest.mark.parametrize("movie", test_data)
+def test_get_movies_by_title(base_url, api_key, movie, request):
+    # Apply marker dynamically
+    request.node.add_marker(getattr(pytest.mark, movie["type"]))
+
+    params = {
+        "apikey": api_key,
+        "t": movie["title"]
     }
-    response = get_movie(base_url,params)
-    body =response.json()
 
-    #schema validation 
-    validate_schema(body,movie["schema"])
+    response = get_movie(base_url, params)
+    body = response.json()
 
-    #business assertions 
+    # 🔑 SCHEMA VALIDATION (data-driven)
+    validate_schema(body, movie["schema"])
+
+    # 🔑 BUSINESS ASSERTION
     assert body["Response"] == movie["expected_response"]
 
-    #error validation (only if expected )
-    if movie ["expected_response"]== "False":
+    # 🔑 ERROR MESSAGE CHECK (only when expected)
+    if movie["expected_response"] == "False":
         assert body["Error"] == movie["expected_error"]
-   
